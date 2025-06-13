59d6ef5e9eb28edc28656af8eb62765797ab3b4a
ZerGio23 <sergio.apaza1432@gmail.com>
2025-06-13 07:02:12 -0400

    fix: join both dashboard in one

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -9,11 +9,11 @@ export const navItems = [
   // { key: 'account', title: 'Cuenta', href: paths.dashboard.account, icon: 'user' },
   // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 
-  // { key: 'profile', title: 'Perfil', href: paths.dashboard.profile, icon: 'user'},
-  // { key: 'users', title: 'Usuarios', href: paths.dashboard.users, icon: 'users'},
-  // { key: 'drivers', title: 'Conductores', href: paths.dashboard.registerDriver, icon: 'plugs-connected'},
-  // { key: 'routes', title: 'Rutas y líneas', href: paths.dashboard.routes, icon: 'plugs-connected'},
-  // { key: 'assignments', title: 'Asignaciones', href: paths.dashboard.assignments, icon: 'gear-six'},
+  { key: 'profile', title: 'Perfil', href: paths.dashboard.profile, icon: 'user'},
+  { key: 'users', title: 'Usuarios', href: paths.dashboard.users, icon: 'users'},
+  { key: 'drivers', title: 'Conductores', href: paths.dashboard.registerDriver, icon: 'plugs-connected'},
+  { key: 'routes', title: 'Rutas y líneas', href: paths.dashboard.routes, icon: 'plugs-connected'},
+  { key: 'assignments', title: 'Asignaciones', href: paths.dashboard.assignments, icon: 'gear-six'},
 
   { key: 'profile', title: 'Perfil', href: paths.dashboard.drivers.profile, icon: 'user'},
   { key: 'assignments', title: 'Asignaciones', href: paths.dashboard.drivers.assignments, icon: 'users'},
